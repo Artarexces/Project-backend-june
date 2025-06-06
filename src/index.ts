627dd3650c2a7c53428363021d90ab83454fa00f
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-06-05 22:00:46 -0300

    index fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
-import cors from "cors";
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./config/db";
+import router from "./routes/booksRoutes";
 
 
 const PORT = process.env.PORT || 2828
@@ -11,14 +11,21 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
+    origin: 'http://localhost:5173'
 }))
 
-// app.use()
+app.use('/api', router)
 
-connectDB()
+app.use('/', (req,res) =>{
+    res.json({ success: true, message:'API biblioteca' })
+})
+
+// Maenjo de errores
+app.use((req, res)=>{
+    res.status(404).json({ success: false, message: 'Ruta no encontrada'})
+})
+
 app.listen(PORT, () => {
+    connectDB()
     console.log(`🚀 Servidor en escucha http://localhost:${PORT}`);
 })
--- src/routes/booksRoutes.ts
+++ src/routes/booksRoutes.ts
@@ -7,4 +7,6 @@ router.get('/books', getAllBooks);
 router.get('/books/:id', getBookById);
 router.post('/books', postBook);
 router.patch('/books/:id', editBookById);
-router.delete('/books/:id', deleteBookById)
+router.delete('/books/:id', deleteBookById);
+
+export default router
